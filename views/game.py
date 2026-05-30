1fb362837bd204b8a4fdd51648946a51df9b4f8a
Braxlysh <demonfather666888999@gmail.com>
2026-05-30 15:07:28 +0300

    game

--- views/game.py
+++ views/game.py
@@ -10,15 +10,28 @@ class GameView(arcade.View):
     def __init__(self):
         super().__init__()
         arcade.set_background_color(arcade.color.SKY_BLUE)
-    
+
+    def load_sprite_lists(self):
+        # self.player_list = arcade.SpriteList()
+        # self.enemy_list = arcade.SpriteList()
+        self.water_list = self.tile_map.sprite_lists["Water"]
+        self.road_list = self.tile_map.sprite_lists["Road"]
+
+
     def setup(self):
-        pass
+        self.tile_map = arcade.load_tilemap(
+            "assets/maps/Main.tmx",
+            # scaling=TILE_SCALE,
+        )
+        self.load_sprite_lists()
 
     def on_draw(self):
         self.clear()
-    
+        self.water_list.draw()
+        self.road_list.draw()
+
     def on_update(self, delta_time):
-        pass
+        self.water_list.update_animation(delta_time)
     
     def on_key_press(self, symbol, modifiers):
         pass
